9ed1cf2cfc5534938994cc59c0c83d7aa719c85c
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2022-08-05 00:59:58 -0400

    Update AuthController.java

--- src/main/java/com/revature/dummyapp/controllers/AuthController.java
+++ src/main/java/com/revature/dummyapp/controllers/AuthController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import com.revature.dummyapp.services.TokenService;
  */
 
 @RestController
+@CrossOrigin
 @RequestMapping(path="/auth")
 public class AuthController {
 	
